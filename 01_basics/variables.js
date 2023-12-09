2af1cc1c0d88751b8ab74724f9896d5f6a255079
Bhavesh Rawat <124232012+Yokohama01@users.noreply.github.com>
2023-12-09 09:17:54 +0000

    Variables learn

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 14421
+let accountEmail = "suresh12@gmail.com"
+var accountPassword ="12345"
+accountCity = "Dehradun"
+accountEmail ="hc@gmail.com"
+accountPassword="2222"
+accountCity = "Bangluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
